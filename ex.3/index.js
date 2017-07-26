8f3be5e15253e9a4a13cfdca536987e17a3da1d3
Renato Asterio da Costa Araujo <renatoasterio@Macbook-Renato.local>
2017-07-26 00:05:27 -0300

    aula 14 udeny

---
+++ ex.3/index.js
@@ -0,0 +1,7 @@
+import 'react'
+
+export default props => (
+    <h1>Ola mundo.</h1>
+)
+
+console.log('funcionou..')
--- ex/index.js
+++ ex/index.js
@@ -1,6 +1,1 @@
-const logger = require('./logger');
-logger.info('usando o padrão CommonJS!');
-const pessoa = new Pessoa('Renato Asterio');
-console.log(pessoa.toString());
-
-import Pessoa from './pessoa'
+import './estilo.css'
