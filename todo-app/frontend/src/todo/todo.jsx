f056f3b59b9009073e610cd1e64f25ca20dab875
unknown <renato.asterio@outlook.com>
2017-08-20 14:59:42 -0300

    .

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,25 +1,52 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+
+        this.refresh();
     }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"> </PageHeader>
-                <TodoForm handleAdd={this.handleAdd}/>
-                <TodoList />
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
+                <TodoList list={this.state.list}
+                handleRemove={this.handleRemove} />
             </div>
         )
     }
 
-    handleAdd(){
-        
+    refresh() {
+        Axios.get(URL, `${URL}?sort=-createdAt`).then(
+            (resp) => this.setState({ ...this.state, description: '', list: resp.data }));
+    }
+
+    handleAdd() {
+        const description = this.state.description;
+        Axios.post(URL, { description }).then(resp => this.refresh())
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value });
+    }
+
+    handleRemove(todo) {
+        Axios.delete(`${URL}/${todo._id}`)
+        .then(resp => this.refresh());
     }
 }
--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -5,10 +5,12 @@ import IconButtom from '../template/iconButtom'
 export default props =>(
     <div role="form" className="todoForm">
         <Grid cols="12 9 10">
-            <input id="description" className="form-control" placeholder="Adicione uma tarefa"></input> 
+            <input id="description" className="form-control" placeholder="Adicione uma tarefa"
+            value={props.description}
+            onChange={props.handleChange}></input> 
         </Grid>
         <Grid cols="12 3 2">
-            <IconButtom style="primary" icon="plus" />
+            <IconButtom style="primary" icon="plus" onClick={props.handleAdd} />
         </Grid>
     </div>
 )
