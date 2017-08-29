6b769fa19a2600d9834b8f988f3a678e700e5f2c
unknown <renato.asterio@outlook.com>
2017-08-29 02:45:51 -0300

    ..

---
+++ todo-app/frontend-redux/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"> </PageHeader>
+        <h2>Nossa hístorio</h2>
+        <p>Lorem ipsum dolor</p>
+        <h2>Nossa Visão</h2>
+        <p>Lorem ipsum dolor</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor</p>
+    </div>
+)   
