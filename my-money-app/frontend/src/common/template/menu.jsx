7d40ef224e8e55d6d27b4a23769cb3e6610067d1
Renato Asterio da Costa Araujo <renato.asterio@outlook.com>
2018-08-04 08:07:07 -0300

    mm

---
+++ my-money-app/frontend/src/common/template/menu.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default prop =>(
+    <ul className="sibebar-menu">
+        
+    </ul>
+)
--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,7 +1,13 @@
+import '../common/template/dependencies'
+
 import React from 'react'
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+
 
 export default props =>(
     <div className="wrapper">
-        <h1>App</h1>
+        <Header />
+        <SideBar />
     </div>
 )
